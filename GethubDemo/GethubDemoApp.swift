3b9996daf2429a9c0f28dc72c55eb0192277ddbb
Tala155 <talaabdullah2017@hotmail.com>
2024-09-26 17:06:02 +0300

    Initial Commit

---
+++ GethubDemo/GethubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GethubDemoApp.swift
+//  GethubDemo
+//
+//  Created by Tala Abdullah on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GethubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GethubDemoTests/GethubDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GethubDemoTests.swift
+//  GethubDemoTests
+//
+//  Created by Tala Abdullah on 23/03/1446 AH.
+//
+
+import Testing
+@testable import GethubDemo
+
+struct GethubDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
